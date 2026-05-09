ae8f6c6c68fbcfced280a1837d28c68b48febf2e
Thabang Tisane <thabangtisane@Thabangs-MacBook-Air.local>
2026-05-09 18:06:24 +0200

    Push calculator updates to section-three branch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 
-
+import React from 'react';
+import HexCalculator from './components/HexCalculator';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <main className="App-main">
+        <HexCalculator />
+      </main>
     </div>
   );
 }
